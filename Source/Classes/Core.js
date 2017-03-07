2b1c6a60c005199078d23cef3fd942c3008ff714
Adrian Preuß <info@adi-code.de>
2017-03-07 03:59:50 +0100

    Rename fix
    using `git config core.ignorecase false`

---
+++ Source/Classes/Core.js
@@ -0,0 +1,55 @@
+(function Core() {
+	this.init = function init() {
+		var gui	= require('nw.gui');
+		var win	= gui.Window.get();
+		
+		document.addEventListener('click', function onClick(event) {
+			if(!event) {
+				event = window.event;
+			}
+			
+			var parent = Tools.getClosest(event.target, '[data-action]');
+			
+			if(typeof(parent) != 'undefined' && typeof(parent.dataset) != 'undefined' && typeof(parent.dataset.action) != 'undefined') {
+				switch(parent.dataset.action) {
+					case 'window:close':
+						win.close(true);
+					break
+					case 'window:maximize':
+						document.querySelector('button[data-action="window:maximize"]').dataset.action = 'window:restore';
+						win.maximize();
+					break;
+					case 'window:restore':
+						win.restore();
+						document.querySelector('button[data-action="window:restore"]').dataset.action = 'window:maximize';
+					break;
+					case 'window:minimize':
+						win.minimize();
+					break;
+					case 'settings:save':
+					case 'settings:close':
+					case 'settings:toggle':
+						var element	= document.querySelector('overlay[data-view="settings"]');
+						element.classList.toggle('show');
+						this.checkOverlay();
+					break;
+				}
+			}
+		}.bind(this));
+		
+		this.checkOverlay();
+	};
+	
+	this.checkOverlay = function checkOverlay() {
+		var elements	= document.querySelectorAll('overlay');
+		document.body.dataset.overlay = null;
+		
+		[].forEach.call(elements, function(element) {
+			if(element.classList.contains('show')) {
+				document.body.dataset.overlay = 'settings';
+			}
+		});
+	};
+	
+	this.init();
+}());
